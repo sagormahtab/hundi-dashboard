49784153654e4de70f3c0bebe7322f5957a577e8
sagormahtab <sagormahtab@gmail.com>
2021-11-15 23:11:22 +0600

    organized project structures

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,22 @@
 <script>
   import { Router, Link, Route } from "svelte-routing";
-  import Dashboard from "./components/Dashboard.svelte";
   import Header from "./components/Header.svelte";
-  import Login from "./components/Login.svelte";
+  import Sidebar from "./components/Sidebar.svelte";
+  import Home from "./pages/Home.svelte";
+  import Login from "./pages/Login.svelte";
 
   export let url = "";
 </script>
 
 <main>
+  <Header />
+  <Sidebar />
   <Router {url}>
     <div>
       <Route exact path="/">
         <div class="container-fluid">
           <div class="row">
-            <Header />
-            <Dashboard />
+            <Home />
           </div>
         </div>
       </Route>
--- src/components/Dashboard.svelte
+++ src/components/Dashboard.svelte
@@ -1,7 +1,165 @@
 <script>
-  import Sidebar from "./Sidebar.svelte";
-  import Home from "./Home.svelte";
+  import { afterUpdate, onMount } from "svelte";
+  import { PlusSquareIcon } from "svelte-feather-icons";
+  import axios from "axios";
+  import Pagination from "./Pagination.svelte";
+  import TableHead from "./TableHead.svelte";
+  import NumbersTableBody from "./NumbersTableBody.svelte";
+  import TableSkeleton from "./TableSkeleton.svelte";
+  import { BASE_SERVER } from "../constants/urls";
+  import NumbersForm from "./NumbersForm.svelte";
+
+  let numbers = [];
+  let paginationInfo = {
+    hasPrevPage: false,
+    hasNextPage: false,
+    page: 1,
+    limit: 10,
+    totalPages: 1,
+  };
+  let isLoading = true;
+
+  let modalInfo = {
+    isModalOpen: false,
+    modalHeaderText: "",
+    type: "",
+    number: "",
+    paymentMethod: "Choose an option...",
+    limit: "",
+  };
+
+  const theaders = ["Sl No", "Phone No", "Payment Method", "Limit"];
+
+  const handlePaginate = async (e) => {
+    if (e.detail.type === "incDec") {
+      paginationInfo.page = paginationInfo.page + e.detail.value;
+    } else if (e.detail.type === "direct") {
+      paginationInfo.page = e.detail.value;
+    }
+    await getNumbers();
+  };
+
+  const getNumbers = async () => {
+    try {
+      const res = await axios.get(
+        `${BASE_SERVER}/api/numbers?limit=${paginationInfo.limit}&page=${paginationInfo.page}`
+      );
+      numbers = res.data.docs;
+      const { hasPrevPage, hasNextPage, page, limit, totalPages } = res.data;
+      paginationInfo = {
+        hasPrevPage,
+        hasNextPage,
+        page,
+        limit,
+        totalPages,
+      };
+      isLoading = false;
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
+  const handleAddNew = () => {
+    modalInfo = {
+      type: "add",
+      number: "",
+      paymentMethod: "Choose an option...",
+      limit: "",
+      modalHeaderText: "Add Info",
+      isModalOpen: !modalInfo.isModalOpen,
+    };
+  };
+
+  const handleModalClose = () => {
+    modalInfo.isModalOpen = false;
+  };
+
+  const handleChangeNumberInit = (e) => {
+    modalInfo = {
+      type: "edit",
+      isModalOpen: !modalInfo.isModalOpen,
+      modalHeaderText: "Edit Info",
+      id: e.detail.id,
+      number: e.detail.number,
+      paymentMethod: e.detail.paymentMethod,
+      limit: e.detail.limit,
+    };
+  };
+
+  const handleActiveNumber = (e) => {
+    axios
+      .put(`${BASE_SERVER}/api/toggle/${e.detail.id}`, {
+        active: !e.detail.active,
+      })
+      .then((res) => {
+        const itemIndex = numbers.findIndex((num) => num._id === e.detail.id);
+        numbers[itemIndex].active = res.data.active;
+      });
+  };
+
+  const handleEditNumber = (e) => {
+    const itemIndex = numbers.findIndex((num) => num._id === e.detail.num._id);
+    numbers[itemIndex] = e.detail.num;
+  };
+
+  const handleNewNumber = (e) => {
+    numbers = [...numbers, e.detail.num];
+  };
+
+  const handleDeleteNumber = (e) => {
+    axios
+      .delete(`${BASE_SERVER}/api/numbers/${e.detail.id}`)
+      .then((res) => {
+        console.log(res.data);
+        const itemIndex = numbers.findIndex((num) => num._id === e.detail.id);
+        numbers.splice(itemIndex, 1);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  onMount(getNumbers);
 </script>
 
-<Sidebar />
-<Home />
+<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+  <div class="">
+    <h1 class="h4 mt-3 text-center">
+      Welcome <span class="text-info">user</span>, to your dashboard
+    </h1>
+    <div
+      class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"
+    >
+      <h2 class="mt-4 h4">List of numbers</h2>
+      <button
+        type="button"
+        class="btn btn-sm btn-outline-secondary"
+        on:click={handleAddNew}><PlusSquareIcon class="me-1" />Add New</button
+      >
+    </div>
+
+    <div class="table-responsive">
+      <table class="table table-striped table-sm">
+        <TableHead {theaders} />
+        {#if isLoading}
+          <TableSkeleton />
+        {:else}
+          <NumbersTableBody
+            {numbers}
+            on:editnumberinit={handleChangeNumberInit}
+            on:activenumber={handleActiveNumber}
+            on:deletenumber={handleDeleteNumber}
+          />
+        {/if}
+      </table>
+    </div>
+    <NumbersForm
+      {numbers}
+      {modalInfo}
+      on:closemodal={handleModalClose}
+      on:newnumber={handleNewNumber}
+      on:editnumber={handleEditNumber}
+    />
+    <Pagination on:paginate={handlePaginate} {paginationInfo} />
+  </div>
+</main>
